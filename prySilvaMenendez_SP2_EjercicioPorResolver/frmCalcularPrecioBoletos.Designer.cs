01aa1e9dee20c5882066320f170b3b9d0bc2ff3d
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-26 08:42:45 -0300

    Fuente Mas Grande, Etiqueta Precio

--- prySilvaMenendez_SP2_EjercicioPorResolver/frmCalcularPrecioBoletos.Designer.cs
+++ prySilvaMenendez_SP2_EjercicioPorResolver/frmCalcularPrecioBoletos.Designer.cs
@@ -35,6 +35,9 @@
             this.txtDias = new System.Windows.Forms.NumericUpDown();
             this.txtDistancia = new System.Windows.Forms.MaskedTextBox();
             this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblKilometros = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.txtDias)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,45 +54,82 @@
             // lblDistancia
             // 
             this.lblDistancia.AutoSize = true;
-            this.lblDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDistancia.Location = new System.Drawing.Point(55, 147);
+            this.lblDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDistancia.Location = new System.Drawing.Point(48, 111);
             this.lblDistancia.Name = "lblDistancia";
-            this.lblDistancia.Size = new System.Drawing.Size(240, 17);
+            this.lblDistancia.Size = new System.Drawing.Size(271, 20);
             this.lblDistancia.TabIndex = 1;
             this.lblDistancia.Text = "Ingrese la Distancia Hasta el Destino";
             // 
             // lblDias
             // 
             this.lblDias.AutoSize = true;
-            this.lblDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDias.Location = new System.Drawing.Point(55, 252);
+            this.lblDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDias.Location = new System.Drawing.Point(48, 212);
             this.lblDias.Name = "lblDias";
-            this.lblDias.Size = new System.Drawing.Size(260, 17);
+            this.lblDias.Size = new System.Drawing.Size(293, 20);
             this.lblDias.TabIndex = 2;
             this.lblDias.Text = "Ingrese la Cantidad de Dias de Estancia";
             // 
             // txtDias
             // 
-            this.txtDias.Location = new System.Drawing.Point(349, 249);
+            this.txtDias.Location = new System.Drawing.Point(365, 212);
+            this.txtDias.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
             this.txtDias.Name = "txtDias";
             this.txtDias.Size = new System.Drawing.Size(55, 20);
             this.txtDias.TabIndex = 3;
             // 
             // txtDistancia
             // 
-            this.txtDistancia.Location = new System.Drawing.Point(325, 144);
+            this.txtDistancia.Location = new System.Drawing.Point(365, 111);
+            this.txtDistancia.Mask = "99999";
             this.txtDistancia.Name = "txtDistancia";
-            this.txtDistancia.Size = new System.Drawing.Size(100, 20);
+            this.txtDistancia.Size = new System.Drawing.Size(55, 20);
             this.txtDistancia.TabIndex = 4;
+            this.txtDistancia.ValidatingType = typeof(int);
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(438, 370);
+            this.btnCalcular.Location = new System.Drawing.Point(429, 289);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(75, 23);
             this.btnCalcular.TabIndex = 5;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // lblKilometros
+            // 
+            this.lblKilometros.AutoSize = true;
+            this.lblKilometros.Location = new System.Drawing.Point(426, 116);
+            this.lblKilometros.Name = "lblKilometros";
+            this.lblKilometros.Size = new System.Drawing.Size(23, 13);
+            this.lblKilometros.TabIndex = 6;
+            this.lblKilometros.Text = "KM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Precio del Boleto";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(268, 361);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(51, 20);
+            this.lblPrecio.TabIndex = 8;
+            this.lblPrecio.Text = "label2";
+            this.lblPrecio.Visible = false;
             // 
             // frmCalcularPrecioBoletos
             // 
@@ -97,6 +137,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LimeGreen;
             this.ClientSize = new System.Drawing.Size(591, 457);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblKilometros);
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.txtDistancia);
             this.Controls.Add(this.txtDias);
@@ -121,6 +164,9 @@
         private System.Windows.Forms.NumericUpDown txtDias;
         private System.Windows.Forms.MaskedTextBox txtDistancia;
         private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblKilometros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPrecio;
     }
 }
 
